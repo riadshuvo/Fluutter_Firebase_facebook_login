b27217a7dbbb3b26b0f450d68fd7c1e9c71cb408
riadshuvo <riadshuvo55@gmail.com>
2020-12-10 21:29:58 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/facebook_login_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.facebook_login_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:facebook_login_firebase/pages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Facebook Login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginWithFacebook(),
+    );
+  }
+}
